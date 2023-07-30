4d912cfba268190ef52d10ea9771ec7a0e764061
Jadielly Lima <m.jadiellydlima@gmail.com>
2023-07-30 09:41:41 -0300

    conexão com o banco de dados

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,37 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/**modulo de conexão**/
+	//parametros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/dbagenda?useTimeZone=true&serverTimeZone=UTC";
+	private String user = "root";
+	private String password = "Jade@12345";
+	
+	//método de conexão
+	
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,user,password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	//teste de conexão
+	public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
