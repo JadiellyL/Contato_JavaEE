54738cdb3e15d0729ebcd5cd80dd04329f76013d
Jadielly Lima <m.jadiellydlima@gmail.com>
2023-08-08 18:06:43 -0300

    Adicionando dump da base de dados dbagenda

---
+++ dbagenda.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: dbagenda
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.22.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contatos`
+--
+
+DROP TABLE IF EXISTS `contatos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contatos` (
+  `idcontato` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `fone` varchar(15) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idcontato`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contatos`
+--
+
+LOCK TABLES `contatos` WRITE;
+/*!40000 ALTER TABLE `contatos` DISABLE KEYS */;
+INSERT INTO `contatos` VALUES (2,'Josafa Martins','8888-99999','josafa@email.com'),(3,'Maria Julia Lima','7777-3333','julia@email.com'),(5,'Mariana Silverio','8888-33333','mariana@email.com'),(6,'Suzana Moreira','33333-3333','suzana@email.com'),(7,'Jadielly Dias','9999-9999','jadielly@email.com'),(8,'Rosimeire Sampaio','7777-8888','rosimeire@email.com'),(9,'Josafa dos Santos','10101010101','josa@email.com'),(10,'Marisol Luz','7777-7777','marisol@email.com');
+/*!40000 ALTER TABLE `contatos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-08 18:02:50
